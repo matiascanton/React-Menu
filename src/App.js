6e7a14280e10bc3f94d1489deadb5c5a92078761
matiascanton <matiasacanton@gmail.com>
2020-12-17 20:14:57 -0300

    menu y filtros

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Menu from './components/Menu';
+import Categories from './components/Categories';
+import items from './components/data';
+
+// Obtenemos todas las categorias de data.js
+// Como vienen repetidas conel new Set obtenemos solo una de c/u
+// y como necesitas agregar 'all', entonces encerramos todo en un arreglo
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 
 function App() {
+
+  const [menuItems, setMenuItems] = useState(items);
+
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2> Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   );
 }
 
